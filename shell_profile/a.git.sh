7bccb16288c96512f917d5a4035905e783190ebf
AbhishekKr <abhikumar163@gmail.com>
2013-04-11 14:02:59 +0530

    git reset author

--- shell_profile/a.git.sh
+++ shell_profile/a.git.sh
@@ -11,3 +11,4 @@ alias gpr='git pull --rebase'
 alias glp='git log -p'
 alias ggl="git log --graph --pretty=format:'%C(red)%h%Creset %C(bold yellow)%s%n %Cblue%an%Cred %C(green)%cd'"
 alias gglp="git log -p --graph --pretty=format:'%C(red)%h%Creset %C(bold yellow)%s%n %Cblue%an%Cred %C(green)%cd'"
+alias git_reset_author='git commit --amend --reset-author'
