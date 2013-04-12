757cbcdfad5d7458ee5fa7cf771a390d586a4fdb
AbhishekKr <abhikumar163@gmail.com>
2013-04-12 22:20:11 +0530

    shell_profile clone-github-private

--- shell_profile/a.github.sh
+++ shell_profile/a.github.sh
@@ -32,3 +32,26 @@ clone-github-to-contribute(){
     sed -i 's/git\:\/\/github\.com\//git@github.com:/' $1/.git/config
   fi
 }
+
+#old github style # no contrib
+# usgae:
+#   $ clone-github-private github.private_instance.com mr_x
+clone-github-private(){
+  GITHUB_HOST=$1
+  GITHUB_ID=$2
+  repos=`curl -skL https://$GITHUB_HOST/$GITHUB_ID | grep 'title="Forks"' | sed "s/.*$GITHUB_ID\///" | sed 's/\/network"\stitle="Forks">//'`
+  for line in `echo $repos | xargs -L1`;
+  do
+    if [ ! -z $line ];
+    then
+      repo_git='git://'$GITHUB_HOST'/'$GITHUB_ID'/'$line'.git'
+      if [ -e $line ]; then
+        echo 'Fetching master latest pull for: '$repo_git
+        cd $line ; git pull ; cd -
+      else
+        echo "Cloning... "$repo_git
+        `git clone $repo_git`
+      fi
+    fi
+  done
+}
