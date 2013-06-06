f77862354895e47b06807a8986011350495124f4
AbhishekKr <abhikumar163@gmail.com>
2013-06-06 19:41:03 +0530

    git profile alias for upstream fetch and merge

--- shell_profile/a.git.sh
+++ shell_profile/a.git.sh
@@ -12,3 +12,5 @@ alias glp='git log -p'
 alias ggl="git log --graph --pretty=format:'%C(red)%h%Creset %C(bold yellow)%s%n %Cblue%an%Cred %C(green)%cd'"
 alias gglp="git log -p --graph --pretty=format:'%C(red)%h%Creset %C(bold yellow)%s%n %Cblue%an%Cred %C(green)%cd'"
 alias git_reset_author='git commit --amend --reset-author'
+
+alias git_upstream_sync_master='git fetch upstream ; git merge upstream/master'
