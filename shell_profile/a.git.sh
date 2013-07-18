1dbc88b9ab07cc0441a33cde7ceff252a6a541b8
AbhishekKr <abhikumar163@gmail.com>
2013-07-18 23:29:50 +0530

    adding some git magic from talk 'Git & GitHub Secrets' by Zach :clap:

--- shell_profile/a.git.sh
+++ shell_profile/a.git.sh
@@ -5,7 +5,9 @@ alias gad='git add -p'
 alias gcm='git commit -m'
 alias gcp='git clone'
 alias gst='git status'
+alias gsb='git status -sb'
 alias gdf='git diff'
+alias gdw='git diff --word-diff'
 alias gdc='git diff --cached'
 alias gpr='git pull --rebase'
 alias glp='git log -p'
@@ -14,4 +16,23 @@ alias gglp="git log -p --graph --pretty=format:'%C(red)%h%Creset %C(bold yellow)
 alias git_reset_author='git commit --amend --reset-author'
 
 alias git_upstream_sync_master='git fetch upstream ; git merge upstream/master'
-alias git_authors="git shortlog -sn --all"
+alias git_authors='git shortlog -sn'
+alias git_authors_all="git shortlog -sn --all"
+alias git_undo='git reset --soft HEAD^1'
+alias git_amend='git commit --amend'
+
+git_credit(){
+  if [ $# -ne 2 ]; then
+    echo "Syntax: <cmd> Name id@email"
+  else
+    git credit "$1" $2
+  fi
+}
+
+git_commit_as(){
+  if [ $# -ne 2 ]; then
+    echo "Syntax: <cmd> Name id@email"
+  else
+    git commit --amend --author "${1} <${2}>" -C HEAD
+  fi
+}
