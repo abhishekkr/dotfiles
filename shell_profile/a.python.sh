91f1bb00c36a951a1b6b3bf7a23dabf11d6053bd
AbhishekKr <abhikumar163@gmail.com>
2013-09-25 08:17:27 +0530

    venv_on and venv_off added to python profile for local virtualenv easy usage

--- shell_profile/a.python.sh
+++ shell_profile/a.python.sh
@@ -7,3 +7,10 @@ alias server.py='python2 -m SimpleHTTPServer'
 export PIP_DOWNLOAD_CACHE=$HOME/cache/pip
 
 alias pyibook='ipython2 notebook'
+
+venv_on(){
+  [ ! -s ./.venv ] && virtualenv .venv
+  source .venv/bin/activate
+}
+
+alias venv_off="deactivate"
