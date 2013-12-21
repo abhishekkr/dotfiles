278aeacef0a53b9b212ba7e4269a4e7a577b1f2d
AbhishekKr <abhikumar163@gmail.com>
2013-12-21 16:29:26 +0530

    [shell_profile][cli] ls-net-svc, enable_ip_forwarding, drop_cache, server.https
    and ASCII ART for PacMan and MaxPayne

--- shell_profile/a.cli.sh
+++ shell_profile/a.cli.sh
@@ -110,8 +110,76 @@ alias espeakf="espeak -ven-us+f2"
 alias 0ssh='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 alias 0scp='scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
+alias ls-net-svc="netstat -plut"
+
 export PATH=$PATH:$HOME\bin
 
 alias prompt_time='PROMPT="%K%B%t "'
 
 alias rsync_to="rsync -lavzh  --exclude .git ./"
+
+alias enable_ip_forwarding="echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+alias drop_cache="echo 3 | sudo tee /proc/sys/vm/drop_caches > /dev/null"
+
+alias server.https="python -m SimpleHTTPServer & ncat --ssl -l 8443 --sh-exec \"ncat 127.0.0.1 8000\" --keep-open"
+
+ascii_pacman(){
+    if [[ $1 != "" ]]; then
+      _msg=$1
+    else
+      _msg=`date`
+    fi
+    _asciiart_pacman="__________________|      |____________________________________________";
+    _asciiart_pacman=$_asciiart_pacman"\n     ,--.    ,--.          ,--.   ,--.";
+    _asciiart_pacman=$_asciiart_pacman"\n    |oo  | _  \  \`.       | oo | |  oo|";
+    _asciiart_pacman=$_asciiart_pacman"\no  o|~~  |(_) /   ;       | ~~ | |  ~~|o  o  o  ${_msg} o  o";
+    _asciiart_pacman=$_asciiart_pacman"\n    |/\/\|   '._,'        |/\/\| |/\/\|";
+    _asciiart_pacman=$_asciiart_pacman"\n__________________        ____________________________________________";
+    _asciiart_pacman=$_asciiart_pacman"\n__________________|      |____________________________________________";
+    echo "$_asciiart_pacman"
+}
+
+ascii_maxpayne(){
+    if [[ $1 != "" ]]; then
+      _msg=$1
+    else
+      _msg=`date`
+    fi
+_asciiart_maxpayne=""
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                         ..sSsSSSSSSb.                                       "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                       .SSSSSSSSSSSSSSS.                                     "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                    .SSSSSSSSSSSSSSSSSSSSSb.                                 "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                  .SSSSSSSSSSSSSSSSSSSSSSSSS                                 "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                 SSSSSSSSSSSSSSSSSS\'  \`SSSS                                 "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                 SSSSSSSSSSSSSSS\'        SSS                                 "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                 SSSSSSSSSSSSS\'         \`SS.                                "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                \`SSSSSSSSSSSSS          \`SSS.                              "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                  \`SSSSSSSSS\'       .sSSS SS S                              "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                     SSSSSSSSS.sSs .sSSs\"   s s                              "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                      SSSSSSSSSSSS           SP                              "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                     \`SSSSSSSSSSSs          S                               "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                        SSSSSSSSSSS.    \",                                   "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n                       \`SSSSSSSSSSsSS                                       "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n        sSSS.           \`SSSSSSSSSSSS.s\"\'   .S.                             "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n        SSSSS.             \`SSSSSSSSSS.    .SSSSs.sSs.                      "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n         SSSSS.             \`SSSSSSSSSP   .SSSSSSSSSSSS.                    "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n         SSSSSS.              \`SSSSSSS\' .SSSSSSSSSSSSSSSS.                  "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n         \`SSSSSS.                 SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSs.          "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n           SSSSSS.               \`SSSSSSSSS SSSSSSSSSSSSSSSSSSSSSSSSs.      "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n         .sSSSSSSS.                \`SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS.    "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n         s  SSSSSSS.                .SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS.   "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n         \`SSSSSSSSSS.             .SSS\' SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS.  "
+_asciiart_maxpayne=$_asciiart_maxpayne"\n          \`SSSSSSSSSS.           sS\'   SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS."
+_asciiart_maxpayne=$_asciiart_maxpayne"\n          SSSSSSSSSSSSe         SSS     \`SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+_asciiart_maxpayne=$_asciiart_maxpayne"\n        .\' SSSSSSSSSS7         SSSS       \`SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+_asciiart_maxpayne=$_asciiart_maxpayne"\n       \"   \`SSSSSSSS7         SSSSS       .SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+_asciiart_maxpayne=$_asciiart_maxpayne"\n      SSSs..SSSSSSS7        SSSSSSS    .sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+_asciiart_maxpayne=$_asciiart_maxpayne"\n       SSSSSSSSSSSS        SSSSSSSS .sSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+_asciiart_maxpayne=$_asciiart_maxpayne"\n        SSSSSSSSSSS     .SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+_asciiart_maxpayne=$_asciiart_maxpayne"\n      .SSSSSSSSSSSSS   .SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+_asciiart_maxpayne=$_asciiart_maxpayne"\n      SSSSSSSSSSSSSS  .SSSSSSSSSSS SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS"
+echo $_asciiart_maxpayne
+}
+
+cowsay -f dragon "$USER is here..."
