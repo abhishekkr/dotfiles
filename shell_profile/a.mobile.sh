d99ab0b0989b8053ed55a95efb4a21eb158a9f8b
AbhishekKr <abhikumar163@gmail.com>
2013-12-31 18:47:08 +0530

    [shell][mobile] some qik mobile aliases, to upgrade

--- shell_profile/a.mobile.sh
+++ shell_profile/a.mobile.sh
@@ -2,5 +2,29 @@
 
 alias ruboto='jruby -S ruboto'
 
+[ -z $Android_SDK_tools ] && export PATH=$PATH:$Android_SDK_tools
+
 export ANDROID_LOCATION=/opt
 export ANDROID_HOME=$ANDROID_LOCATION/android-sdk
+
+[ -d "${ANDROID_HOME}/tools" ] && export PATH=$PATH:$ANDROID_HOME/tools
+
+alias android-avd="android avd &"
+
+
+##### PhoneGap: http://phonegap.com/
+
+alias phonegap-get="npm install -g phonegap"
+
+alias phonegap-new="phonegap create"
+
+alias phonegap-droid="phonegap run android"
+
+
+##### Apache Cordova: http://cordova.apache.org/
+
+alias cordova-get="npm install -g cordova"
+
+alias cordova-new="phonegap create"
+
+alias cordova-droid="cordova platform add android ; cordova emulate android"
