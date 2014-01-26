6445d8ff6bcec4866ac60c5a6893cb1e459da6d2
AbhishekKr <abhikumar163@gmail.com>
2014-01-26 20:18:56 +0530

    xopen() added to media shell profile for verbose xdg-open (|||mac's open)

--- shell_profile/a.media.sh
+++ shell_profile/a.media.sh
@@ -22,3 +22,17 @@ media2avi(){
 
   ffmpeg -i "$source_media" -qscale 0 "$dest_media"
 }
+
+xopen(){
+  xdg-open "$@" ; _TMP_EXITCODE=$?
+  if [ "${_TMP_EXITCODE}" == "1" ]; then
+    echo "[ERROR:] Error in command line syntax"
+  elif [ "${_TMP_EXITCODE}" == "2" ]; then
+    echo "[ERROR:] One of the files passed on the command line did not exist"
+  elif [ "${_TMP_EXITCODE}" == "3" ]; then
+    echo "[ERROR:] A required tool could not be found"
+  elif [ "${_TMP_EXITCODE}" == "4" ]; then
+    echo "[ERROR:] The action failed"
+  fi
+  return $_TMP_EXITCODE
+}
