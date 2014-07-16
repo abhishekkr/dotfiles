7a9c44ea55f76261a93d2c3d8c266867166c9838
AbhishekKr <abhikumar163@gmail.com>
2014-07-16 23:09:47 +0530

    shell-git gco, ga, git_workdays, git_submodule_r, also some refaktor

--- shell_profile/a.git.sh
+++ shell_profile/a.git.sh
@@ -1,38 +1,43 @@
 #profile for git
 
-alias gad='git add'
+alias ga='git add'
 alias gad='git add -p'
+
 alias gcm='git commit -m'
 alias gcm0='git commit --allow-empty -m'
+
 alias gcp='git clone'
+
+alias gco='git checkout'
 
 alias gdf='git diff'
 alias gdw='git diff --word-diff'
 alias gdc='git diff --cached'
+
 alias gpr='git pull --rebase'
 alias gpull='git pull'
 alias gpullo='git pull origin'
+
 alias gpush='git push'
 alias gpusho='git push origin'
 alias git_upstream_sync_master='git fetch upstream ; git merge upstream/master'
 
 alias gst='git status'
 alias gsb='git status -sb'
+
 alias gbrr='git branch -r'
+
 alias glp='git log -p'
 alias ggl="git log --graph --pretty=format:'%C(red)%h%Creset %C(bold yellow)%s%n %Cblue%an%Cred %C(green)%cd'"
 alias gglp="git log -p --graph --pretty=format:'%C(red)%h%Creset %C(bold yellow)%s%n %Cblue%an%Cred %C(green)%cd'"
-
 alias git_authors='git shortlog -sn'
 alias git_authors_all="git shortlog -sn --all"
+alias git_workdays="git log --date=short --format="%ci"|awk '{print $1}'|uniq"
+alias git_submodule_r="git submodule update --init --recursive"
 
 alias git_undo='git reset --soft HEAD~1'
 alias git_reset_author='git commit --amend --reset-author'
 alias git_amend='git commit --amend'
-
-alias git_workdays="git log --date=short --format="%ci"|awk '{print $1}'|uniq"
-
-alias git_submodule_r="git submodule update --init --recursive"
 
 gpull_all(){
   for _a in `ls` ; do
@@ -72,20 +77,9 @@ git_commit_as(){
   fi
 }
 
-gitinit(){
-  for REPO_TO_INIT in $@; do
-    if [ -d "${PWD}/${REPO_TO_INIT}/.git" ]; then
-      echo "${PWD}/${REPO_TO_INIT} is already a git repo."
-    else
-      echo "initializing git repo: ${PWD}/${REPO_TO_INIT}"
-      git init $REPO_TO_INIT
-      cd $REPO_TO_INIT
-      cat >> ./README.md << READMEEOF
-##${REPO_TO_INIT}
----
-
-READMEEOF
-      cat >> ./.gitignore << GITIGNORE
+git-ignore(){
+  if [[ $# -eq 0 ]]; then
+    cat >> ./.gitignore << GITIGNORE
 *swn
 *swo
 *swp
@@ -100,6 +94,37 @@ temp/*
 *.lock
 *.sock
 GITIGNORE
+  else
+    for _TO_IGNORE in $@; do
+      cat "$_TO_IGNORE" >> ./.gitignore
+    done
+  fi
+}
+
+git-readme(){
+  _ABOUT_IT="$@"
+  if [[ $# -eq 0 ]]; then
+    _README_FOR=`basename $PWD`
+  fi
+  cat >> ./README.md << READMEEOF
+##${_README_FOR}
+---
+
+${_ABOUT_IT}
+
+READMEEOF
+}
+
+gitinit(){
+  for REPO_TO_INIT in $@; do
+    if [ -d "${PWD}/${REPO_TO_INIT}/.git" ]; then
+      echo "${PWD}/${REPO_TO_INIT} is already a git repo."
+    else
+      echo "initializing git repo: ${PWD}/${REPO_TO_INIT}"
+      git init $REPO_TO_INIT
+      cd $REPO_TO_INIT
+      git-readme
+      git-ignore
       cd -
     fi
   done
