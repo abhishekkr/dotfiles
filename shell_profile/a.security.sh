caea70b25d619051554478ce9db6c79993d234dd
AbhishekKr <abhikumar163@gmail.com>
2016-03-09 01:56:36 +0530

    [shell_profile] security: ipaddr-public, gen-secure,no-history

--- shell_profile/a.security.sh
+++ shell_profile/a.security.sh
@@ -1,5 +1,13 @@
 ## net/sec
 
+gen-secure(){
+  openssl rand -base64 512 | tr -d '\r\n' | tee "$1"
+}
+
+nohistry(){
+  export HISTFILE=/dev/null
+}
+
 my-public-ip(){
   local _MY_PUBLIC_IP=$( curl "https://www.google.co.in/search?q=my+ip&oq=my+ip&aqs=chrome..69i57.1205j0j1&sourceid=chrome&es_sm=0&ie=UTF-8" 2>/dev/null | grep -E -o '\(Client IP address: [0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*\)' | grep -E -o '([0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*)' )
   if [[ "${_MY_PUBLIC_IP}" == "" ]]; then
@@ -9,6 +17,7 @@ my-public-ip(){
     echo "Public IP: ${ABK_MY_PUBLIC_IP}"
   fi
 }
+alias ipaddr-public="my-public-ip"
 
 nmap-xtreme(){
   sudo nmap -p 1-65535 -sS -sU -T4 -A -vvvv -Pn -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script "default or (discovery and safe)" -oA "nmap-$1" $1
