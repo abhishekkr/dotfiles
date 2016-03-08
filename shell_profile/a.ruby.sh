bec72779d0f471cd86d5d40ee7dad7b5de98ad1e
AbhishekKr <abhikumar163@gmail.com>
2016-03-09 01:55:56 +0530

    [shell_profile] ruby: rb,berb,beirb,beirbr

--- shell_profile/a.ruby.sh
+++ shell_profile/a.ruby.sh
@@ -1,9 +1,13 @@
 # profile for ruby
 
-alias r='/usr/bin/env ruby'
+alias rb='ruby'
+
 alias bi='bundle install'
 alias bib='bundle install --path .bundle'
 alias be='bundle exec'
+alias berb='bundle exec ruby'
+alias beirb='bundle exec irb -I .'
+alias beirbr='bundle exec irb -I . -r'
 
 alias gem-i="gem install"
 alias gem-u="gem uninstall"
