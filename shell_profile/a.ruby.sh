cd0fb78aba0d3fbd8a1e477ead67262b4d5af8a9
AbhishekKr <abhikumar163@gmail.com>
2016-03-18 12:56:48 +0530

    [shell_profile] making dot-ruby-version-2-2-3 alias

--- shell_profile/a.ruby.sh
+++ shell_profile/a.ruby.sh
@@ -13,3 +13,8 @@ alias gem-i="gem install"
 alias gem-u="gem uninstall"
 
 alias server.rb='ruby -run -e httpd . -p5000'
+
+dot-ruby-version-2-2-3(){
+  local RUBY_VERSION_FILE="${PWD}/.ruby-version"
+  echo 'ruby-2.2.3' | tee "${RUBY_VERSION_FILE}"
+}
