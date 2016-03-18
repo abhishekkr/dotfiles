dabfb403dbad74e363891452635f5fbb5da7e4c7
AbhishekKr <abhikumar163@gmail.com>
2016-03-18 12:54:38 +0530

    [shell_profile] few qik kitchen aliases

--- shell_profile/a.cfgmgmt.sh
+++ shell_profile/a.cfgmgmt.sh
@@ -9,7 +9,7 @@ cookbook-nu(){
 
 ## generate $cookbook/.chef/... structure for vendor-ized shalreable structure
 dot-chef(){
-  mkdir -p "$PWD/.chef/{cookbooks,nodes,roles,data_bags}"
+  mkdir -p .chef/{cookbooks,nodes,roles,data_bags}
 }
 
 ##################### Berkshelf ##########################
@@ -25,3 +25,18 @@ berks-vendor(){
 ##################### chef-spec ##########################
 alias chef-exec="chef exec"
 alias chef-spec="chef exec rspec spec"
+
+##################### kitchen ############################
+kitchen-exec(){
+  local _KITCHEN_CMD="$@"
+  kitchen exec -c "$_KITCHEN_CMD ; echo"
+}
+kitchen-test(){
+  kitchen test -c
+}
+alias kitchen-recreate="kitchen destroy && kitchen converge"
+alias kitchen-reverify="kitchen destroy && kitchen converge && kitchen verify"
+alias kitchen-create="kitchen create"
+alias kitchen-converge="kitchen converge"
+alias kitchen-destroy="kitchen destroy"
+alias kitchen-verify="kitchen verify"
