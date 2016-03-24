396bdc23e4ae78e0012c7341d445e3d0dd793e0f
AbhishekKr <abhikumar163@gmail.com>
2016-03-24 10:11:46 +0530

    [shell_profile] acpi-status

--- shell_profile/a.monitor.sh
+++ shell_profile/a.monitor.sh
@@ -12,3 +12,12 @@ sysdig-k8(){
   local _KUBERNETES_URI="$1"
   sudo csysdig -pc -k "${_KUBERNETES_URI}"
 }
+
+acpi-status(){
+  local at_time battery temp adapter
+  at_time=$(date)
+  battery=$(acpi -b | awk '{print $4}')
+  temp=$(acpi -t | awk '{print $3,$4}')
+  adapter=$(acpi -a | awk '{print $3}')
+  echo "[ ${at_time} ] Battery: ${battery} | Temp: ${temp} | Adapter: ${adapter}"
+}
