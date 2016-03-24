db1a14c01d0d96c7a99dba731749f0482505d11f
AbhishekKr <abhikumar163@gmail.com>
2016-03-24 10:12:20 +0530

    [shell_profile] a.network.sh introduced - ip-lis-*

---
+++ shell_profile/a.network.sh
@@ -0,0 +1,6 @@
+
+alias ip-ls-bridge="ip link show type bridge"
+alias ip-ls-bond="ip link show type bond"
+alias ip-ls-dummy="ip link show type dummy"
+alias ip-ls-veth="ip link show type veth"
+alias ip-ls-vlan="ip link show type vlan"
