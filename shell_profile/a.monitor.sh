293ca72359faf651fc347750594f1ab4b2b487d2
AbhishekKr <abhikumar163@gmail.com>
2017-02-09 17:45:13 +0530

    [profile]monitor prefix for easy search, added valgrind-massif and pmapx--
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- shell_profile/a.monitor.sh
+++ shell_profile/a.monitor.sh
@@ -1,9 +1,12 @@
 ##################### sysdig ##################################################
 
-alias sysdig-topcontainers-net='sudo sysdig -c topcontainers_net'
-alias sysdig-htop='sudo csysdig -pc'
+alias monitor-sysdig-topcontainers-net='sudo sysdig -c topcontainers_net'
+alias sysdig-topcontainers-net='monitor-sysdig-topcontainers-net'
 
-sysdig-k8(){
+alias monitor-sysdig-htop='sudo csysdig -pc'
+alias sysdig-htop='monitor-sysdig-htop'
+
+monitor-sysdig-k8(){
   if [[ $# -eq 0 ]]; then
     echo "USAGE: sysdig-k8 <KUBERNETES_URI>"
     echo "KUBERNETES_URI need to be K8 API URI similar to 'http://admin:password@127.0.0.1:8080'"
@@ -12,8 +15,9 @@ sysdig-k8(){
   local _KUBERNETES_URI="$1"
   sudo csysdig -pc -k "${_KUBERNETES_URI}"
 }
+alias sysdig-k8="monitor-sysdig-k8"
 
-acpi-status(){
+monitor-acpi-status(){
   local at_time battery temp adapter
   at_time=$(date --rfc-3339='seconds')
   battery=$(acpi -b | awk '{print $4}')
@@ -21,3 +25,26 @@ acpi-status(){
   adapter=$(acpi -a | awk '{print $3}')
   echo "[ ${at_time} ] Battery: ${battery} | Temp: ${temp} | Adapter: ${adapter}"
 }
+alias acpi-status="monitor-acpi-status"
+
+
+##################### valgrind #################################################
+
+monitor-valgrind-massif(){
+  valgrind --tool=massif $@
+  local massif_files=$(ls -laht | grep massif.out | awk '{print $9}' | head -1)
+  echo "lookout for massif.out.\* files created at ${PWD}"
+  echo "can use following to vizualize recordings\n\'\$ massif-visualizer ${massif_files}\'"
+}
+
+
+
+##################### pmap #################################################
+
+alias monitor-pmapd="pmap -d"
+alias monitor-pmapx="pmap -x"
+alias monitor-pmapxx="pmap -XX"
+
+
+###############################################################################
+
