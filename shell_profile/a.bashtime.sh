389fd691799cf191db796d247e3ca159cfc7a748
AbhishekKr <abhikumar163@gmail.com>
2024-04-08 21:21:16 +0530

    add ist-navic for ISRO satellite based Time in IST
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- shell_profile/a.bashtime.sh
+++ shell_profile/a.bashtime.sh
@@ -118,3 +118,20 @@ clock(){
 sudo-here(){
   sudo su -c 'cd $PWD ; bash'
 }
+
+ist-navic(){
+  local VYOM_TIME=$(curl -skL 'https://www.sac.gov.in/Vyom/time' \
+    -H 'accept: */*' \
+    -H 'accept-language: en-US,en;q=0.9' \
+    -H 'referer: https://www.sac.gov.in/Vyom/time_current' \
+    -H 'sec-ch-ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"' \
+    -H 'sec-ch-ua-mobile: ?0' \
+    -H 'sec-ch-ua-platform: "Linux"' \
+    -H 'sec-fetch-dest: empty' \
+    -H 'sec-fetch-mode: cors' \
+    -H 'sec-fetch-site: same-origin' \
+    -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36' \
+    -H 'x-requested-with: XMLHttpRequest')
+  echo $VYOM_TIME | jq '.date'
+  echo $VYOM_TIME | jq '.time'
+}
